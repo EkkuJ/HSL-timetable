1f3cb1fb7615fa1ba92a249769fa55cbe746aa84
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-20 02:16:47 +0200

    Create pipeline to fetch data from API

--- timetable/src/components/App.js
+++ timetable/src/components/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import Plan from './Plan'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-      <body className="App-body">
-        Body
-      </body>
+const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app</h2>
+      <Plan/>
     </div>
-  );
-}
+  </ApolloProvider>
+)
 
-export default App;
+export default App
