31fc85c4fefd3bcfdc6644cac48bef48f9555ab3
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-22 02:26:13 +0200

    Move structure away from App.js

--- timetable/src/components/App.js
+++ timetable/src/components/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
-import Plan from './Plan'
+import InfoScreen from './InfoScreen'
 
+// Apollo Client, uses the Helsinki Region GraphQL-endpoint of digitransit.
 const client = new ApolloClient({
   uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
 })
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo app</h2>
-      <Plan/>
-    </div>
-  </ApolloProvider>
-)
+// The whole app. Wraps an ApolloProvider around the InfoScreen.
+const App = () => {
+  return (<div>
+    <ApolloProvider client={client}>
+      <InfoScreen/>
+    </ApolloProvider>
+  </div>)
+}
 
 export default App
