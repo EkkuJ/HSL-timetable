6c08612c12d4044f99d37d6ea1e88bf1c0b0815e
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-24 01:48:34 +0200

    Add Dockerfile, rebuild

---
+++ timetable/build/precache-manifest.1948b980f66d0e9bce685c998048b878.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f88b73ff22f80d03adec60692473ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2314a2a50f5d592cec1",
+    "url": "/static/js/2.64c4ce65.chunk.js"
+  },
+  {
+    "revision": "c0d1569b2748db0ab52e686ed719bcb0",
+    "url": "/static/js/2.64c4ce65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22faf6d03cc7ce17e82",
+    "url": "/static/js/main.5896cfdb.chunk.js"
+  },
+  {
+    "revision": "d7c8d4a310d5dfc84930",
+    "url": "/static/js/runtime-main.aa265521.js"
+  }
+]);
--- timetable/build/service-worker.js
+++ timetable/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9bffd3b8b3e236d85a1ed7e291603b8.js"
+  "/precache-manifest.1948b980f66d0e9bce685c998048b878.js"
 );
 
 self.addEventListener('message', (event) => {
