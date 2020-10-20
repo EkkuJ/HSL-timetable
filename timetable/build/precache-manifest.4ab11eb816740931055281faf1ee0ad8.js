b05b7c9c33ab9483b925e618c1b4c735cb7b80b7
Ekku Jokinen <ekku.jokinen@rovio.com>
2020-10-20 16:52:56 +0300

    npm build

---
+++ timetable/build/precache-manifest.4ab11eb816740931055281faf1ee0ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9098ebc78f4d22dd07cbd85a4a70b904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b093199fd0f58dce9cca",
+    "url": "/static/js/2.4cd03a87.chunk.js"
+  },
+  {
+    "revision": "953311a3a5fd6d2345ff122bf06e0764",
+    "url": "/static/js/2.4cd03a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d63337cb954cfdbe2c",
+    "url": "/static/js/main.a449a96b.chunk.js"
+  },
+  {
+    "revision": "d7c8d4a310d5dfc84930",
+    "url": "/static/js/runtime-main.aa265521.js"
+  }
+]);
